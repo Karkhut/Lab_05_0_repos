caa58fb37bc6f57289f7847fae45a6c8b2422abe
Demian <dem4ikomg@gmail.com>
2023-10-25 23:21:28 +0300

    first test

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_05_1/main.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		
+				double c = g(1, 1) + pow(1 + pow(g(2, 1), 2), 2) / 1 + pow(g(1 + 2, 1), 3);
+				Assert::AreEqual(c, 4700.0);
+
+		}
+	};
+}
